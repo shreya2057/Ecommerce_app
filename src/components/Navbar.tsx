a88ee29f2e37fb65a6054d51ca7191887885a6fd
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2024-01-03 15:02:47 +0545

    Navbar made responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,79 @@
 import {
     Box,
     Flex,
-    Stack,
-    Text
+    IconButton,
+    Text,
+    useBreakpointValue
 } from "@chakra-ui/react";
 import { HiOutlineUserCircle, HiOutlineShoppingCart } from "react-icons/hi";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
 function NavBar(pt: {loggedIn: Boolean}){
     const username = "harry potter";
+    const [showNavMenu, setShowNavMenu] = useState(false)
+    const menuVisibility = useBreakpointValue({"base": !showNavMenu, "md": false})
+    const buttonVisibility = useBreakpointValue({"sm": false, "md": true})
+    const toggleMenu = ()=>{
+        if(showNavMenu){
+            setShowNavMenu(false);
+        } else{
+            setShowNavMenu(true)
+        }
+    }
     return(
-        <Flex direction={"row"} width={"100%"} bgColor={"#653059"} px={10} py={2} justifyContent={"space-between"} align={"center"}>
-            <Link to={"/"}>
-                <Text 
-                    fontSize={"2xl"} 
-                    fontWeight={"bold"} 
-                    textColor={"#fffdf0"}
-                >
-                    Ecommerce App
-                </Text>
-            </Link>
-            <Flex gap={6} height={"100%"} fontSize={"xl"} fontWeight={"bold"} textColor={"#fffdf0"} align={"center"}>
+        <Flex 
+            direction={{"base": "column", "md": "row"}} 
+            width={"100%"} 
+            bgColor={"#653059"}
+            py={{"md":2}}            
+            justifyContent={"space-between"} 
+            align={{"base":"start","md":"center"}}
+        >
+            <Flex gap={2} px={10} py={{"base":3, "md":0}} >
+                <IconButton 
+                    hidden={buttonVisibility}
+                    bgColor={"#b87c94"} 
+                    textColor={"#fffdf0"} 
+                    aria-label="Toggle Navigation" 
+                    icon={<HamburgerIcon/>}
+                    _focus={{
+                        backgroundColor: "#b87c94"
+                    }}  
+                    _active={{
+                        backgroundColor: "#b87c94"
+                    }}  
+                    _hover={{
+                        backgroundColor: "#b87c94"
+                    }}  
+                    onClick={toggleMenu}
+                />
+                <Link to={"/"}>
+                    <Text 
+                        fontSize={"2xl"} 
+                        fontWeight={"bold"} 
+                        textColor={"#fffdf0"}
+                    >
+                        Ecommerce App
+                    </Text>
+                </Link>
+            </Flex>
+            <Flex 
+                flex={{"sm": 1, "md":0}}
+                width={{"sm": "100%"}}
+                hidden= {menuVisibility}
+                bgColor={{"base": "#b87c94", "md": "inherit"}}
+                direction={{"base": "column", "md": "row"}} 
+                gap={{"base": 1,"md":6}} 
+                height={"100%"} 
+                fontSize={"xl"} 
+                fontWeight={"bold"} 
+                textColor={"#fffdf0"}
+                px={{"sm": 10, "md":10}} 
+                py={{"sm": 3, "md":0}} 
+                align={{"base":"start","md":"center"}}
+            >
                 <Link to={"/"}>
                     <Text>
                         Shopping
@@ -34,7 +87,11 @@ function NavBar(pt: {loggedIn: Boolean}){
                 {
                     !pt.loggedIn
                     &&
-                    <Stack direction={"row"} gap={6}>
+                    <Flex 
+                        direction={{"base": "column", "md": "row"}} 
+                        gap={{"base": 1,"md":6}}
+                        align={{"base":"start","md":"center"}}
+                    >
                         <Link to={"/"}>
                             <Text>
                                 Login
@@ -45,12 +102,17 @@ function NavBar(pt: {loggedIn: Boolean}){
                                 Signup
                             </Text>
                         </Link>
-                    </Stack>
+                    </Flex>
                 }
                 {
                     pt.loggedIn
                     &&
-                    <Flex direction={"row"} gap={6} align={"center"}>
+                    <Flex 
+                        direction={{"base": "column", "md": "row"}}
+                        flex={1} 
+                        gap={{"base": 1,"md":6}}
+                        align={{"base":"start","md":"center"}}
+                    >
                         <Link to={"/"}>
                             <Box display={"flex"} gap={2}>
                                 <Box display={"flex"} alignSelf={"center"}>
@@ -60,11 +122,22 @@ function NavBar(pt: {loggedIn: Boolean}){
                             </Box>
                         </Link>
                         <Link to={"/"}>
-                            <Box display={"flex"} mt={0} gap={1.5} borderWidth={0} bgColor={"#b87c94"} py={1} px={2} rounded={"sm"} shadow={"md"}>
+                            <Box 
+                                display={"flex"}
+                                justifyContent={"center"}
+                                width={130}
+                                gap={1.5}  
+                                bgColor={{"md": "#b87c94"}} 
+                                py={{"md":1}} 
+                                px={{"md":2}} 
+                                rounded={{"md": "base"}} 
+                                shadow={{"md":"md"}}
+                                fontSize={{"base":"lg","md":"sm"}}
+                            >
                                 <Box display={"flex"} alignSelf={"center"}>
                                     <HiOutlineUserCircle/>
                                 </Box>
-                                <Text fontSize={"sm"}>{username}</Text>  
+                                <Text fontSize={"base"}>{username}</Text>  
                             </Box>
                         </Link>
                     </Flex>
